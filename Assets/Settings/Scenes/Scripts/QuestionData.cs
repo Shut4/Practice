d0ec1d476c763027ed862fd8839d6d6c3931cb4b
Shut4 <syuta0910a@yahoo.co.jp>
2025-03-31 14:19:55 +0900

    feat: クイズゲームの大枠作成

---
+++ Assets/Settings/Scenes/Scripts/QuestionData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionData : MonoBehaviour
+{
+    public string question;
+    public string answer1;
+    public string answer2;
+    public string answer3;
+    public string answer4;
+    public string correctAnswer;
+}
